ad54da207d6af755628ae6021e7999f37bf59869
slim <slimabida21@gmail.com>
2025-11-17 13:35:21 +0100

    feat: amélioration formulaire de contact
    - Centrage vertical et horizontal du formulaire
    - Correction indentation et structure ContactForm
    - Ajout champ sujet avec sélection
    - Styles améliorés avec backdrop blur et ombres

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,9 @@
 import styles from "./contact.module.css";
+import ContactForm from "@/components/ContactForm/ContactForm";
 export default function Contact() {
-  return <div className={styles.contactContainer}></div>;
+  // création du formulaire de contact côté client //
+  return <div className={styles.contactContainer}>
+    <ContactForm />
+
+  </div>;
 }
--- components/ContactForm/ContactForm.tsx
+++ components/ContactForm/ContactForm.tsx
@@ -1,8 +1,57 @@
-export default function ContactForm() {
-  return (
-    <div>
-      <p>ContactForm</p>
-    </div>
-  )
-}
+"use client";
+import styles from "./contact.module.css";
 
+export default function ContactForm() {
+
+  return (
+    <div className={styles.contactFormContainer}>
+      <form className={styles.contactForm} >
+        <div className={styles.contactFormGroup}>
+          <label htmlFor="name">Nom</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            className={styles.contactFormInput}
+          />
+        </div>
+        <div className={styles.contactFormGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            className={styles.contactFormInput}
+          />
+        </div>
+        <div className={styles.contactFormGroup}>
+          <label htmlFor="subject">Sujet</label>
+          <select
+            id="subject"
+            name="subject"
+            className={styles.contactFormInput}
+          >
+            <option value="Demande de contact">Demande de contact</option>
+            <option value="Demande de collaboration">
+              Demande de collaboration
+            </option>
+            <option value="Demande de concert">Demande de concert</option>
+            <option value="Demande de presse">Demande de presse</option>
+            <option value="Autre">Autre</option>
+          </select>
+        </div>
+        <div className={styles.contactFormGroup}>
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className={styles.contactTextarea}
+          />
+        </div>
+        <button type="submit" className={styles.contactFormButton}>
+          Envoyer
+        </button>
+      </form>
+    </div>
+  );
+}
