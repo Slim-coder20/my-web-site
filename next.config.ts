de2b941f8e4c70d9af8462fc4283c7b4655da84d
slim <slimabida21@gmail.com>
2025-11-17 12:47:32 +0100

    feat: configuration images externes pour next/image
    - Autorisation domaines Amazon, Paniermusique, Cloudinary
    - Support images depuis sources externes pour pochettes albums

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,34 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "m.media-amazon.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.spotifycdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.paniermusique.fr",
+      },
+      {
+        protocol: "https",
+        hostname: "static.fnac-static.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
